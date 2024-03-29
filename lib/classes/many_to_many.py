85e01a6094a5d2ea1a89d24bfea3f7ac5cf7aed2
Yuri <+>
2024-03-28 21:01:56 -0400

    Yuri's P3 Project

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,36 @@
+import ipdb
+
 class Article:
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        pass
+
+    # @property
+    # def title(self):
+    #     return self._title
+    
+    # @title.setter
+    # def title(self, title_parameter):
+    #     print(title_parameter)
+
 class Author:
     def __init__(self, name):
         self.name = name
+        pass
+
+    # @property
+    # def author_name(self):
+    #     return self._name
+    
+    # @author_name.setter
+    # def author(self, author_name_parameter):
+    #     if(type(author_name_parameter, str) and len(author_name_parameter) > 0):
+    #         self._name = author_name_parameter
+    #     else:
+    #         print("Author's name must be a string")
+
 
     def articles(self):
         pass
@@ -24,7 +48,11 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        pass
 
+    # @property
+    # def magazine_name(self):
+        
     def articles(self):
         pass
 
@@ -36,3 +64,5 @@ class Magazine:
 
     def contributing_authors(self):
         pass
+
+ipdb.set_trace()
